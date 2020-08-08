91da32c9ab29c3569991e3fd6bb50f336c500ca1
Dobrica <dobrica@getpenta.com>
2020-08-08 22:57:00 +0200

    try restructuring

--- main.go
+++ main.go
@@ -1,85 +1,30 @@
-package main
+package op
 
 import (
+	"errors"
 	"fmt"
 	"os"
-
-	"github.com/hpdobrica/op/pkg/op"
 )
 
-var testVault string = "Sandbox"
+func Signin(domain, email, secretKey, masterPassword string) error {
 
-func main() {
-	signinAddr := os.Getenv("OP_SIGNIN_ADDRESS")
-	email := os.Getenv("OP_EMAIL")
-	secretKey := os.Getenv("OP_SECRET_KEY")
-	masterPass := os.Getenv("OP_MASTER_PASSWORD")
-	signinErr := op.Signin(signinAddr, email, secretKey, masterPass)
+	versionOk, versionErr := checkOpCliVersion(executor)
 
-	if signinErr != nil {
-		fmt.Println("error while signing in")
-		fmt.Println(signinErr)
-		os.Exit(1)
+	if versionErr != nil {
+		return versionErr
 	}
 
-	fmt.Println("successfully initialized op")
+	if !versionOk {
+		return errors.New("op-cli version must be 1.3.0 or higher")
+	}
 
-	// ExampleListItems()
-	// ExampleGetItem()
-
-	// ExampleListTemplates()
-
-	ExampleListVaults()
-
-}
-
-func ExampleListItems() {
-	items, err := op.ListItems(testVault)
+	token, err := executor.Run(fmt.Sprintf("echo %s | op signin %s %s %s --raw", masterPassword, domain, email, secretKey))
 
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 
-	for _, item := range items {
-		fmt.Println(item.Overview.Title)
-	}
-}
+	os.Setenv("OP_SESSION", token)
 
-func ExampleListTemplates() {
-	templates, err := op.ListTemplates()
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	for _, tpl := range templates {
-		fmt.Println(tpl.Name)
-	}
-
-}
-
-func ExampleGetItem() {
-	items, _ := op.ListItems(testVault)
-
-	item, err := op.GetItem(items[0].Uuid)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Println(item.Details.Fields[0].Value)
-
-}
-
-func ExampleListVaults() {
-	vaults, err := op.ListVaults()
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	for _, vault := range vaults {
-		fmt.Println(vault.Name)
-	}
-
+	return nil
 }
